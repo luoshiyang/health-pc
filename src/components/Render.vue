29f2c79e596e878b2f91289e5316d9448f70e22f
luoshiyang <296733282@qq.com>
2019-02-27 15:16:07 +0800

    dev分支修改表格高度

--- src/components/Render.vue
+++ src/components/Render.vue
@@ -23,7 +23,7 @@ export default {
     props: {
         height: {
             type: Number,
-            default: 400
+            default: 420
         },
         xSplit: {
             type: Number,
@@ -31,7 +31,7 @@ export default {
         },
         heightCount: {
             type: Number,
-            default: 50
+            default: 70
         }
     },
     data() {
