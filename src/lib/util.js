f9245267525b345b96c2cb6ff2545da21b3bf14a
luoshiyang <296733282@qq.com>
2019-05-30 15:16:05 +0800

    优化text 把数字都看作一行进行渲染

--- src/lib/util.js
+++ src/lib/util.js
@@ -178,3 +178,13 @@ export const createDashLine = (
         }
     });
 };
+
+export const checkNumStr = (val) => {
+    let regPos = /^\d+(\.\d+)?$/; //非负浮点数
+    let regNeg = /^(-(([0-9]+\.[0-9]*[1-9][0-9]*)|([0-9]*[1-9][0-9]*\.[0-9]+)|([0-9]*[1-9][0-9]*)))$/; //负浮点数
+    if(regPos.test(val) || regNeg.test(val)) {
+        return true;
+    } else {
+        return false;
+    }
+}
